f1dded79ebb18f321ad53607f9eaff8bbf4e448b
Chen Xu <chenxu@MacBook-Pro.local>
2024-02-01 13:38:09 +0800

    added fcc project

---
+++ fcc/scripts/deploy_storage.js
@@ -0,0 +1,35 @@
+// imports
+const { ethers, run, netowrk } = require("hardhat");
+
+// async main
+async function main() {
+    const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
+    console.log("Deploying contract ...");
+    const simpleStorage = await SimpleStorageFactory.deploy();
+    await simpleStorage.waitForDeployment();
+    console.log(`Deployed contract to: ${simpleStorage.target}`);
+    if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
+        await simpleStorage.deploymentTransaction().wait(6);
+        await verify(simpleStorage.target, []);
+    }
+}
+
+async function verify(contractAddress, args) {
+    console.log("Verifying contract ...");
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        })
+    } catch (e) {
+        if (e.message.toLowerCase().include("already verified")) {
+            console.log("Already verified!")
+        }
+    }
+}
+
+// main
+main().then(() => process.exit(0)).catch((error) => {
+    console.error(error);
+    process.exit(1);
+});
