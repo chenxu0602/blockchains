58d280088ebdfb096d45e0a4c1a0672fe926338f
Chen Xu <chenxu@MacBook-Pro.local>
2024-02-01 16:35:14 +0800

    mod

--- fcc/hardhat.config.js
+++ fcc/hardhat.config.js
@@ -1,10 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 require("./tasks/block-number");
+require("solidity-coverage");
+require("@nomicfoundation/hardhat-verify");
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_PRC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -24,5 +27,13 @@ module.exports = {
   },
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReport: {
+    enabled: true,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    token: "MATIC",
   },
 };
